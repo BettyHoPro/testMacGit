1582cf8516edf229cddc6221a9b91b63f48ea3da
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-10 23:12:49 +0000

    add countOnly.js

---
+++ w1/lotide/countOnly.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+//////////// this time ///////
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+//== (arr, ob)
+
+countOnly = (allItems, itemsToCount) => {
+  const results = {};
+  for (const item of allItems) {
+    console.log(item);
+    //console.log(item + " : "+ itemsToCount[item] );
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+/////// from Compass 
+// ----> if true item in firstname func
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+console.log(firstNames[1]);
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
