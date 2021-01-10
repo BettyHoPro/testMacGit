7708513a8191aa8bdad15b09a52bbf26aacae41e
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-10 20:47:14 +0000

    add assertArraysEqual.js

---
+++ w1/lotide/assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function (arr1, arr2){
+
+  if ( arr1.length !== arr2.length ){
+    return false;
+  } else {
+    for (let q=0 ; q< arr1.length; q++){
+      if ( arr1[q] !== arr2[q]) {
+        return false ;
+      }
+    }
+    return true;
+  }
+}
+
+////////////////////////////
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Array Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Array Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+//assertArraysEqual([1, 4, 3], [1, 2, 3]);
