29dd5867d573894ca58e4d235b8bc5b96aa1a847
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-11 04:21:39 +0000

    add findKey.js

---
+++ w1/lotide/findKey.js
@@ -0,0 +1,56 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+////// this /////
+// let findKey = function (object, callback) {
+//   for (let key in object) {
+    
+//     if (callback(object[key])) {
+//       //console.log(callback(object[key]))
+//       return key;
+//     }
+//   }
+// };
+
+findKey = ( object, callback ) => {
+  for ( let q in object )
+    if ( callback (object[q])){
+      return q;
+    }
+ }
+
+/// example ////
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+assertEqual( 2, 3);
+
+
+////// reference //////
+// findKeyByValue = ( object, value ) => {
+//   for ( let key of Object.keys(object) ){
+//     //console.log(key);
+//     //console.log(object[key]);
+//     if ( object[key] === value) {
+//       return key;
+//     }
+//   }  
+// };
+
+// const bestTVShowsByGenre = { 
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
