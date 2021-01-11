c3b52300aaff83b471e5169be1dc4a761e3d03f2
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-11 03:11:43 +0000

    add mapTest.js

---
+++ w1/d4-funcs/map_builtin/mapTest.js
@@ -0,0 +1,5 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+let lengthmap=lighthouses.map(x=>x.length);
+
+console.log(lengthmap);
